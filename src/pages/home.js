68e547d2f18b15780fad0bc9a62f1bedebbd3dd3
William Reticena <william.wsd43@gmail.com>
2022-03-29 21:10:10 -0300

    :fire: removing files

--- src/pages/home.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export function Home () {
-  return <h1>Você está na HOME!</h1>
-};
--- src/pages/signIn.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export function SignIn () {
-  return <h1>Você está no LOGIN!</h1>
-};
